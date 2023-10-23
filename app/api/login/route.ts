5a791ce39abd5fd2aa55cc6be0a1cf2c3b39ea70
blackbox-user1 <blackbox-vscode-beta@blackboxai.tech>
2023-10-22 21:31:19 -0300

    chore: add a login method

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,6 +1,36 @@
-export async function POST(req: Request) {
-  const body = await req.json();
-  console.log('login', body);
+import bcrypt from 'bcrypt';
 
-  return new Response(JSON.stringify({ id: 1, name: 'J Smith', email: 'jsmith@example.com' }));
+import { getDbClient } from '@/lib/database';
+
+export async function POST(req: Request) {
+  const { username, password } = await req.json();
+  if (!username || !password) {
+    return new Response(null);
+  }
+
+  const connection = await getDbClient();
+
+  const fullUser = await connection.collection('users').findOne({ username });
+
+  if (!fullUser) {
+    return new Response(null);
+  }
+
+  try {
+    const match = await bcrypt.compare(password, fullUser.password);
+    if (!match) {
+      throw new Error('password doenst match');
+    }
+  } catch (error) {
+    console.log('password doenst match');
+    return new Response(null);
+  }
+
+  const user = {
+    id: fullUser.id,
+    name: fullUser.name,
+    email: fullUser.email,
+  };
+
+  return new Response(JSON.stringify(user));
 }
